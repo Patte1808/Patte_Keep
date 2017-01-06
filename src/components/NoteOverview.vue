6b712427c0990c064622929b8c21b0dd32be6c73
Pattelicious <patrick.voss1808@gmail.com>
2017-01-06 11:19:16 +0100

    Something wrong with editing, invastigating later

--- src/components/NoteOverview.vue
+++ src/components/NoteOverview.vue
@@ -3,13 +3,17 @@
     <div class="column is-one-quarter" v-for="(note, id) in notes">
       <div class="box">
         <div class="content">
-          <p>
+          <p class="control" v-if="isEditMode(id)">
+            <input class="input" type="text" v-model="activeNote.title">
+          </p>
+          <p v-else>
             <strong>{{note.title}}</strong>
           </p>
           <nav class="level">
             <div class="level-left">
               <a class="level-item">
-                <a>Edit</a>
+                <a v-if="isEditMode(id)" @click="editNote(id, activeNote)">Save</a>
+                <a @click="setActiveNote(note)" v-else>Edit</a>          
               </a>
               <a class="level-item">
                 <a @click="deleteNote(id)">Delete</a>
@@ -25,14 +29,29 @@
 <script>
 export default {
   name: 'note-overview',
+  data () {
+    return {
+      activeNote: null
+    }
+  },
   computed: {
     notes () {
       return this.$store.getters.notes
     }
   },
   methods: {
+    setActiveNote (note) {
+      this.activeNote = note
+    },
+    editNote (id, activeNote) {
+      this.activeNote = null
+      this.$store.dispatch('editNote', id, activeNote)
+    },
     deleteNote (id) {
       this.$store.dispatch('deleteNote', id)
+    },
+    isEditMode (id) {
+      return this.activeNote !== null && this.activeNote._id === id
     }
   }
 }
--- src/store/store.js
+++ src/store/store.js
@@ -4,12 +4,14 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-  notes: []
+  notes: [],
+  activeNoteEdit: null
 }
 
 const mutations = {
   ADD_NOTE (state, title) {
     const newNote = {
+      _id: (state.notes.length === 0) ? 0 : state.notes[state.notes.length - 1]._id + 1,
       title: title
     }
 
@@ -18,6 +20,14 @@ const mutations = {
 
   DELETE_NOTE (state, id) {
     state.notes.splice(id, 1)
+  },
+
+  EDIT_NOTE (state, id, activeNote) {
+    state.notes[id] = activeNote
+  },
+
+  SET_ACTIVE_NOTE_EDIT (state, note) {
+    state.activeNoteEdit = note
   }
 }
 
@@ -28,12 +38,23 @@ const actions = {
 
   deleteNote ({ commit }, id) {
     commit('DELETE_NOTE', id)
+  },
+
+  editNote ({ commit }, id, activeNote) {
+    commit('EDIT_NOTE', id, activeNote)
+  },
+
+  setActiveNoteEdit ({ commit }, note) {
+    commit('SET_ACTIVE_NOTE_EDIT', note)
   }
 }
 
 const getters = {
   notes (state) {
     return state.notes
+  },
+  activeNoteEdit (state) {
+    return state.activeNoteEdit
   }
 }
 
